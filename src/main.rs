5fe93373f149db7e9402b92378f482685ea11afa
Gregory Laurent <gregory.m.laurent@gmail.com>
2025-02-09 20:12:45 -0800

    Add doc comments for Clock.

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,12 @@ pub mod time {
 
     static FIDELITY: AtomicU64 = AtomicU64::new(0);
 
-    #[derive(Debug, Clone, Copy)]
+    /// Controls the cadence of the clock sweep.
+    /// ```
+    /// // scales each tick to 16 milliseconds.
+    /// Fidelity::Millis(16);
+    /// ```
+    #[derive(Debug, Clone)]
     pub enum Fidelity {
         Nanos(usize),
         Millis(usize),
@@ -52,6 +57,7 @@ pub mod time {
     }
 
     impl Fidelity {
+        /// Calculates the divisor to scale Clock ticks.
         fn divisor(&self) -> u64 {
             match self {
                 Fidelity::Nanos(n) => *n as u64,
@@ -61,12 +67,36 @@ pub mod time {
         }
     }
 
+    /// Clock to read time based on an OS-based value
+    /// when the application starts.
+    ///
+    /// ```
+    /// // OS value set by rust available at runtime.
+    /// const RUN_OS: &str = std::env::consts::OS;
+    /// ```
+    ///
+    /// Darwin => TODO
+    /// Windows => TODO
+    /// Linux => CLOCK_BOOTTIME -- see note on the impl
+    /// why CLOCK_BOOTTIME instead of CLOCK_MONOTONIC
+    ///
+    /// ```
+    /// // These are the same for rnitialization.
+    /// // Clock::new() calls default() and updates
+    /// // fields set by parameters.
+    /// let default = Clock::default();
+    /// let new = Clock::new(Fidelity::Millis(16));
+    ///
+    /// assert_eq!(new.div, default.div, "Fidelity between default() and new() are not the same.");
+    /// ```
     pub struct Clock {
         div: AtomicU64,
         mono: OnceLock<Monotonic>,
     }
 
     impl Default for Clock {
+        /// Initializes the default Clock and setups up time
+        /// based on the detected OS.
         fn default() -> Self {
             let mono: Monotonic = match RUN_OS {
                 LINUX => init::<Linux>(),
@@ -88,6 +118,15 @@ pub mod time {
     }
 
     impl Clock {
+        /// Initializes an new Clock and sets up OS-based time.
+        ///
+        /// ```
+        /// // Clock::new() calls default() and updates
+        /// // fields set from parameters.
+        /// let new = Clock::new(Fidelity::Millis(16));
+        ///
+        /// assert_eq!(new.div, default.div, "Fidelity between default() and new() are not the same.");
+        /// ```
         pub fn new(f: Fidelity) -> Self {
             let mut clock = Clock::default();
             clock.sweep(f);
@@ -95,15 +134,21 @@ pub mod time {
             clock
         }
 
+        /// Update the fidelity (scale) of the tick.
         pub fn sweep(&mut self, f: Fidelity) {
             self.div = AtomicU64::new(f.divisor());
         }
 
+        /// Total number of scaled ticks from application start.
+        ///
+        /// Tick the application clock, then
+        /// OS-based time at application start divided by self.div
         pub fn real_tick(&self) -> u64 {
             self.tick();
             REAL_TIME.load(Ordering::Acquire) / self.div.load(Ordering::Acquire)
         }
 
+        /// Ticks the clock based on elapsed time from applicaiton start.
         pub fn tick(&self) -> u64 {
             let m = match self.mono.get() {
                 Some(t) => t.elapsed(),
@@ -158,13 +203,14 @@ pub mod time {
             Self
         }
 
+        /// Use CLOCK_BOOTTIME instead of CLOCK_MONOTONIC.
+        /// CLOCK_MONOTONIC excludes elapsed time while the system is suspended.
+        ///
+        /// CLOCK_BOOTTIME includes elapsed time while suspended;
+        /// It is the true monotonic clock in Linux.
         fn hw_time(&self) -> u64 {
             assert!(LINUX == RUN_OS, "Failed OS check. Invalid OS path."); // should not happen
 
-            // Use CLOCK_BOOTTIME instead of CLOCK_MONOTONIC
-            // CLOCK_MONOTONIC excludes elapsed time while the system is suspended.
-            // CLOCK_BOOTTIME includes elapsed time while suspended;
-            // It is the true monotonic clock in Linux.
             let time = match nix::time::clock_gettime(nix::time::ClockId::CLOCK_BOOTTIME) {
                 Ok(t) => t,
                 Err(_) => panic!("System CLOCK_BOOTMONO_TIME is required."),
