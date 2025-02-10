2aea34d184dc27277109439e78455db4850e9bdd
Gregory Laurent <gregory.m.laurent@gmail.com>
2025-02-09 17:08:25 -0800

    Refactor and simplify main code base.

--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,48 @@
+#![allow(dead_code, unused)]
+// TODO  Write tests
+// TODO struct Realtime -- let time = match nix::time::clock_gettime(nix::time::ClockId::CLOCK_REALMONO_TIME) {
+// TODO:const WINDOWS: &'static str = "windows"
+// TODO: Crash safely, or recover
+
 use std::time::Duration;
 
-use time::{Fidelity, MonoClock};
+use time::{Clock, Fidelity};
+
+fn main() {
+    let clock = Clock::default();
+
+    loop {
+        let x = clock.tick();
+        println!("{x}");
+
+        std::thread::sleep(Duration::new(2, 0));
+    }
+}
 
 pub mod time {
-    use nix::sys::time::TimeSpec;
-    use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
+    use nix::{libc::adjtime, sys::time::TimeSpec};
+    use std::{
+        env::consts::OS,
+        sync::{
+            atomic::{AtomicU64, Ordering},
+            LazyLock, OnceLock,
+        },
+        u64,
+    };
 
     const LINUX: &str = "linux";
-    //TODO: const WINDOWS: &'static str = "windows";
+    const WINDOWS: &str = "linux"; //FIXME, just for testing
+    const RUN_OS: &str = std::env::consts::OS;
 
     const NANOS_PER_SEC: u64 = 1_000_000_000;
     const NANOS_PER_MILLI: u64 = 1_000_000;
-    const RUN_OS: &str = std::env::consts::OS;
 
-    static RUNNING: AtomicBool = AtomicBool::new(false);
+    static RUN_ON: OnceLock<u64> = OnceLock::<u64>::new();
 
-    static REALTIME: AtomicU64 = AtomicU64::new(0);
-    static REALTIME_GUARD: AtomicU64 = AtomicU64::new(0);
+    static MONO_TIME: AtomicU64 = AtomicU64::new(0);
+    static REAL_TIME: AtomicU64 = AtomicU64::new(0);
 
-    static MONOTONIC: AtomicU64 = AtomicU64::new(0);
-    static MONOTONIC_GUARD: AtomicU64 = AtomicU64::new(0);
-
-    struct Time;
-    impl Time {
-        fn up() {
-            assert!(
-                MONOTONIC_GUARD.load(Ordering::SeqCst) == 0,
-                "Time only needs to be initialized once."
-            );
-
-            assert!(
-                REALTIME_GUARD.load(Ordering::SeqCst) == 0,
-                "Time only needs to be initialized once."
-            );
-
-            RUNNING.store(true, Ordering::SeqCst);
-
-            Self::monotonic();
-
-            Self::realtime();
-        }
-
-        fn monotonic() -> u64 {
-            println!("{:?}", RUNNING.load(Ordering::SeqCst));
-            assert!(
-                RUNNING.load(Ordering::SeqCst),
-                "Time must be initialized with Time::new()."
-            );
-
-            let new = match RUN_OS {
-                LINUX => Self::monotonic_linux(),
-                _ => panic!("Unsupported OS -- Please use Linux"),
-            };
-
-            let _ = MONOTONIC.swap(new, Ordering::SeqCst);
-
-            // TODO: Crash safely, or recover
-            // Hardware and kernel bugs may regress the monotonic clock.
-            // Crash safely rather than possibly creating infinite loops.
-            assert!(
-            MONOTONIC_GUARD.load(Ordering::SeqCst) <= MONOTONIC.load(Ordering::SeqCst)
-                && MONOTONIC.load(Ordering::SeqCst) <= new,
-                "Hardware/kernel bug regressed the monotonic clock. Crashing is better than infinity."
-            );
-
-            new
-        }
-
-        fn realtime() -> u64 {
-            assert!(
-                RUNNING.load(Ordering::SeqCst),
-                "Time must be initialized with Time::new()."
-            );
-
-            let new = match RUN_OS {
-                LINUX => Self::realtime_unix(),
-                _ => panic!("Unsupported OS -- Please use Linux"),
-            };
-
-            let _ = REALTIME.swap(new, Ordering::SeqCst);
-
-            // TODO: Crash safely, or recover
-            // Hardware and kernel bugs may prevent checking CLOCK_REALTIME.
-            // Crash safely rather than being able to read time.
-            assert!(
-            REALTIME_GUARD.load(Ordering::SeqCst) <= REALTIME.load(Ordering::SeqCst)
-                && REALTIME.load(Ordering::SeqCst) <= new,
-                "Hardware/kernel bug regressed the monotonic clock. Crashing is better than infinity."
-            );
-
-            new
-        }
-
-        pub fn elapsed() -> u64 {
-            let now = Self::monotonic();
-            now - MONOTONIC_GUARD.load(Ordering::SeqCst)
-        }
-
-        pub fn elapsed_from(from: u64) -> u64 {
-            let now = Self::elapsed();
-            now - from - MONOTONIC_GUARD.load(Ordering::SeqCst)
-        }
-
-        fn monotonic_linux() -> u64 {
-            assert!(LINUX == RUN_OS, "Failed OS check. Invalid OS path."); // should not happen
-
-            // Use CLOCK_BOOTTIME instead of CLOCK_MONOTONIC
-            // CLOCK_MONOTONIC excludes elapsed time while the system is suspended.
-            // CLOCK_BOOTTIME includes elapsed time while suspended;
-            // It is the true monotonic clock in Linux.
-            let time = match nix::time::clock_gettime(nix::time::ClockId::CLOCK_BOOTTIME) {
-                Ok(t) => t,
-                Err(_) => panic!("System CLOCK_BOOTTIME is required."),
-            };
-
-            Self::adjust_time(time)
-        }
-
-        fn realtime_unix() -> u64 {
-            assert!(LINUX == RUN_OS, "Failed OS check. Invalid OS path."); // should not happen
-            let time = match nix::time::clock_gettime(nix::time::ClockId::CLOCK_REALTIME) {
-                Ok(t) => t,
-                Err(_) => panic!("System CLOCK_REALTIME is required."),
-            };
-
-            let rt = Self::adjust_time(time);
-            REALTIME.store(rt, Ordering::SeqCst);
-
-            rt
-        }
-
-        const fn adjust_time(time: TimeSpec) -> u64 {
-            let secs = time.tv_sec();
-            let nsec = time.tv_nsec();
-
-            // Account for fractional time with nanoseconds; avoids trimming scaled time;
-            let t = secs as u128 * NANOS_PER_SEC as u128 + nsec as u128;
-
-            t as u64
-        }
-    }
+    static FIDELITY: AtomicU64 = AtomicU64::new(0);
 
     #[derive(Debug, Clone, Copy)]
     pub enum Fidelity {
@@ -148,81 +51,140 @@ pub mod time {
         Seconds(usize),
     }
 
-    #[derive(Debug, Clone, Copy)]
-    pub struct MonoClock {
-        started_at: u64,
-        stopped_at: Option<u64>,
-        fidelity: Fidelity,
-    }
-
-    impl MonoClock {
-        pub fn new(fidelity: Fidelity) -> Self {
-            if !RUNNING.load(Ordering::SeqCst) {
-                Time::up();
-            }
-
-            Self {
-                fidelity,
-                started_at: Time::monotonic(),
-                stopped_at: None,
-            }
-        }
-
-        pub fn started_at(&self) -> u64 {
-            assert!(self.started_at > 0);
-
-            self.started_at
-        }
-
-        pub fn stop(&mut self) {
-            self.stopped_at = Some(Time::monotonic());
-        }
-
-        pub fn stopped_at(&self) -> Option<u64> {
-            self.stopped_at
-        }
-
-        pub fn ticks(&self) -> u64 {
-            if self.started_at == 0 {
-                return 0;
-            }
-
-            Self::ticks_with(self.started_at, &self.fidelity)
-        }
-
-        pub fn epoch(&self) -> u64 {
-            Self::ticks_with(0, &self.fidelity)
-        }
-
-        fn ticks_with(from: u64, fidelity: &Fidelity) -> u64 {
-            let div = match fidelity {
+    impl Fidelity {
+        fn divisor(&self) -> u64 {
+            match self {
                 Fidelity::Nanos(n) => *n as u64,
                 Fidelity::Millis(m) => *m as u64 * NANOS_PER_MILLI,
                 Fidelity::Seconds(s) => *s as u64 * NANOS_PER_SEC,
-            };
-
-            let now = Time::elapsed_from(from);
-
-            now / div
+            }
         }
     }
-}
 
-fn main() {
-    let dur = Duration::from_secs(1);
+    pub struct Clock {
+        div: AtomicU64,
+        mono: OnceLock<Monotonic>,
+    }
 
-    let max = 100;
-    let mut count = 0;
-    let m = MonoClock::new(Fidelity::Millis(16));
+    impl Default for Clock {
+        fn default() -> Self {
+            let mono: Monotonic = match RUN_OS {
+                LINUX => init::<Linux>(),
+                _ => panic!("blah"),
+            };
 
-    loop {
-        println!("Clock: {m:?} TICKS {:?}", m.ticks());
+            let mono_lock = OnceLock::new();
+            let m = mono_lock.get_or_init(|| mono);
 
-        count += 1;
-        if count > max {
-            break;
+            let m_time = m.hw_time();
+            RUN_ON.get_or_init(|| m_time);
+            MONO_TIME.swap(m_time, Ordering::AcqRel);
+
+            Clock {
+                div: AtomicU64::new(Fidelity::Millis(16).divisor()),
+                mono: mono_lock,
+            }
+        }
+    }
+
+    impl Clock {
+        pub fn new(f: Fidelity) -> Self {
+            let mut clock = Clock::default();
+            clock.sweep(f);
+
+            clock
         }
 
-        std::thread::sleep(dur);
+        pub fn sweep(&mut self, f: Fidelity) {
+            self.div = AtomicU64::new(f.divisor());
+        }
+
+        pub fn real_tick(&self) -> u64 {
+            self.tick();
+            REAL_TIME.load(Ordering::Acquire) / self.div.load(Ordering::Acquire)
+        }
+
+        pub fn tick(&self) -> u64 {
+            let m = match self.mono.get() {
+                Some(t) => t.elapsed(),
+                None => panic!("fix this"),
+            };
+
+            m / self.div.load(Ordering::Acquire)
+        }
+    }
+
+    fn init<O: OsType>() -> Monotonic {
+        // other time type initialization
+        Monotonic::new()
+    }
+
+    trait OsType {}
+
+    struct Linux;
+    impl OsType for Linux {}
+
+    trait OsTime<O: OsType> {
+        fn new() -> Self;
+
+        fn hw_time(&self) -> u64;
+
+        fn adjust_time(time: TimeSpec) -> u64 {
+            let secs = time.tv_sec();
+            let nsec = time.tv_nsec();
+
+            // Account for fractional time with nanoseconds;
+            // Avoids trimming scaled time.
+            let t = secs as u128 * NANOS_PER_SEC as u128 + nsec as u128;
+
+            t as u64
+        }
+
+        fn elapsed(&self) -> u64 {
+            let t = self.hw_time() - RUN_ON.get().unwrap();
+            self.set_global(t);
+
+            t
+        }
+
+        fn set_global(&self, t: u64);
+    }
+
+    struct Monotonic;
+    impl OsTime<Linux> for Monotonic {
+        fn new() -> Self {
+            assert!(RUN_OS == LINUX, "blah");
+
+            Self
+        }
+
+        fn hw_time(&self) -> u64 {
+            assert!(LINUX == RUN_OS, "Failed OS check. Invalid OS path."); // should not happen
+
+            // Use CLOCK_BOOTTIME instead of CLOCK_MONOTONIC
+            // CLOCK_MONOTONIC excludes elapsed time while the system is suspended.
+            // CLOCK_BOOTTIME includes elapsed time while suspended;
+            // It is the true monotonic clock in Linux.
+            let time = match nix::time::clock_gettime(nix::time::ClockId::CLOCK_BOOTTIME) {
+                Ok(t) => t,
+                Err(_) => panic!("System CLOCK_BOOTMONO_TIME is required."),
+            };
+
+            let time = Self::adjust_time(time);
+            let old = REAL_TIME.swap(time, Ordering::AcqRel);
+
+            // Hardware and kernel bugs may regress the monotonic clock.
+            // Crash safely rather than possibly creating infinite loops.
+            assert!(
+                old <= time,
+                "Hardware/kernel bug regressed the monotonic clock. Crashing is better than infinity."
+            );
+
+            time
+        }
+
+        fn set_global(&self, t: u64) {
+            let _ = MONO_TIME.swap(t, Ordering::AcqRel);
+        }
     }
 }
